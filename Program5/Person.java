527d3e3a27c54c646f4f4888b5d33fc252d33728
EshaPuthran <pputhranesha@gmail.com>
2025-09-08 09:17:34 +0530

    Add files via upload

---
+++ Program5/Person.java
@@ -0,0 +1,17 @@
+package ticketCounter;
+//person class
+public class Person {
+	private String name;
+    private int age;
+    private Gender gender;
+//assigning  fields
+    Person(String name, int age, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    void displayPerson() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Gender: " + gender);
+    }
+
+}
