a2b0daf9d176a47a1699fedcaa54bbcc8d8cb0c6
EshaPuthran <pputhranesha@gmail.com>
2025-10-06 19:12:25 +0530

    Add files via upload

---
+++ Program7/Consumer.java
@@ -0,0 +1,17 @@
+package lab7;
+
+public class Consumer extends Thread {
+	 private MessageBoard board;
+
+	    public Consumer(MessageBoard b) {
+	        this.board = b;
+	    }
+
+	    @Override
+	    public void run() {
+	        String msg;
+	        while (!(msg = board.get()).equals("DONE")) {
+	            System.out.println("Consumer reads: " + msg);
+	        }
+	    }
+	}
